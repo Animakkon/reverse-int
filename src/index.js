a8073ed6e8479f1888f9555ad6f30896581e9caa
Animakkonu <lada.dunskaya@mail.ru>
2021-02-25 19:35:30 +0300

    feat: done reverse int function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+
 module.exports = function reverse (n) {
-  
+    let number = "";
+    n < 0 ? n = n * -1: n;
+    
+    while(Math.floor(n % 10) > 0 || n > 0){
+        number += Math.floor(n % 10);
+        n = (n - Math.floor(n % 10)) / 10;
+    };
+return parseInt(number);    
 }
